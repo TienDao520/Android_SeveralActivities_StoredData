d8393acb2c3fdf01cba0c421a278ea75908a8ac3
Tien Dao <tiendao5240@gmail.com>
2022-02-09 22:23:19 -0500

    Task1-Bundle: Get values from radio buttons

--- app/src/main/java/tdao/example/android_severalactivities_storeddata/MainActivity.kt
+++ app/src/main/java/tdao/example/android_severalactivities_storeddata/MainActivity.kt
@@ -2,10 +2,33 @@ package tdao.example.android_severalactivities_storeddata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RadioGroup
 
 class MainActivity : AppCompatActivity() {
+
+    /**No1-0: Get values from radio buttons*/
+    // good to initialize variables here when used in several functions
+    lateinit var radioGroup: RadioGroup
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // initialize the lateinit variables
+        radioGroup=findViewById(R.id.radioGroup)
+    }
+
+    fun onClick(view: View) {
+        /**No1-0: Get values from radio buttons*/
+        var radValue:Int =0
+        // get the value of the checked radio button
+        var checkedRad:Int = radioGroup.checkedRadioButtonId
+        when(checkedRad) {
+            R.id.radioButton1-> radValue = 1
+            R.id.radioButton2-> radValue = 2
+            R.id.radioButton3-> radValue = 3
+            R.id.radioButton4-> radValue = 4
+        }
+
     }
 }
