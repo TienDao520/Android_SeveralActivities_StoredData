b1f9cf0384ccb040b0fe35c1bce0f061c84c3e44
Tien Dao <tiendao5240@gmail.com>
2022-02-09 22:28:53 -0500

    Task1-Bundle: Get data from other activities in 2nd activity

--- app/src/main/java/tdao/example/android_severalactivities_storeddata/SecondActivity.kt
+++ app/src/main/java/tdao/example/android_severalactivities_storeddata/SecondActivity.kt
@@ -2,10 +2,30 @@ package tdao.example.android_severalactivities_storeddata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var spinner: Spinner
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        // getting the bundle back from the android
+        val bundle = intent.extras
+
+        /**No1-2: where we get data from other activities*/
+        // performing the safety null check
+        var s:String? = "test"
+        // getting the string back, notice the second parameter is the default
+        s = bundle!!.getString("key1", "Default")
+        // getting the int back, notice the second parameter is the default
+        var i:Int = bundle.getInt("key2", 4)
+        // set the textView to the name passed in through the bundle
+        var tv: TextView = findViewById(R.id.textViewNameSecond)
+        tv.setText(s)
+        // set the spinner to the value of the radio button passed in through the bundle
+        spinner = findViewById(R.id.spinner)
+        spinner.setSelection(i-1)
     }
 }
