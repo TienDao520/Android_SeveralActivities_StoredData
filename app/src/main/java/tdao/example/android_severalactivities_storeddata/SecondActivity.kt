49ff910d7ed2051b85c99391d40941dbd8e387a3
Tien Dao <tiendao5240@gmail.com>
2022-02-09 22:17:38 -0500

    Task1-Bundle: Added Second Activity

---
+++ app/src/main/java/tdao/example/android_severalactivities_storeddata/SecondActivity.kt
@@ -0,0 +1,11 @@
+package tdao.example.android_severalactivities_storeddata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
